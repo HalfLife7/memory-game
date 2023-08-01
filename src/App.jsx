13d5b4eeba6a6e052de7c4b111b123e840b4c506
HalfLife7 <d.nguyen@vehikl.com>
2023-07-31 21:22:32 -0400

    cleanup code, switch back to online mode (fetch from API)

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,36 @@ import {useEffect, useState} from 'react'
 import React from "react";
 import './App.css'
 
-const imageFiles = [
-    "images/2RnEtbW.png",
-    "images/3x8hEE1.png",
-    "images/4P0CqRN.jpg",
-    "images/04afe1y.jpg",
-    "images/7P0xXKY.jpg",
-    "images/8EEfLuB.jpeg",
-    "images/8m-r1_O.png",
-    "images/-VVdn7B.png",
-    "images/anKsYF2.png",
-    "images/bBTiHba.png",
-    "images/bodE1ZR.png",
-    "images/fuGfYQJ.jpg",
-    "images/Ju8JMcj.jpg",
-    "images/KfZyMS3.jpg",
-    "images/KjyZfjn.jpg",
-    "images/lMiXE7j.png",
-    "images/mbQ4c4V.jpg",
-    "images/mJkPaVR.png",
-    "images/O4gqsyo.jpg",
-    "images/Ojsl-2a.jpg",
-    "images/rF-pZ8a.jpg",
-    "images/wlvCPrF.jpg",
-    "images/wPbusA9.png",
-    "images/XcpL3nR.jpg",
-    "images/yn_F4Nt.jpeg",
-    "images/ynInTFV.jpg",
-    "images/yYcF1Me.png",
-    "images/z1rMzBR.jpg",
-]
+// const imageFiles = [
+//     "images/2RnEtbW.png",
+//     "images/3x8hEE1.png",
+//     "images/4P0CqRN.jpg",
+//     "images/04afe1y.jpg",
+//     "images/7P0xXKY.jpg",
+//     "images/8EEfLuB.jpeg",
+//     "images/8m-r1_O.png",
+//     "images/-VVdn7B.png",
+//     "images/anKsYF2.png",
+//     "images/bBTiHba.png",
+//     "images/bodE1ZR.png",
+//     "images/fuGfYQJ.jpg",
+//     "images/Ju8JMcj.jpg",
+//     "images/KfZyMS3.jpg",
+//     "images/KjyZfjn.jpg",
+//     "images/lMiXE7j.png",
+//     "images/mbQ4c4V.jpg",
+//     "images/mJkPaVR.png",
+//     "images/O4gqsyo.jpg",
+//     "images/Ojsl-2a.jpg",
+//     "images/rF-pZ8a.jpg",
+//     "images/wlvCPrF.jpg",
+//     "images/wPbusA9.png",
+//     "images/XcpL3nR.jpg",
+//     "images/yn_F4Nt.jpeg",
+//     "images/ynInTFV.jpg",
+//     "images/yYcF1Me.png",
+//     "images/z1rMzBR.jpg",
+// ]
 
 function App() {
     const [images, setImages] = useState([]);
@@ -45,32 +45,43 @@ function App() {
     };
 
     // Fetching images from API
-    // const apiUrl = 'https://api.waifu.pics/many/sfw/waifu';
-    // var formdata = new FormData();
-    // formdata.append("exclude", "[]");
-    //
-    // var requestOptions = {
-    //     method: 'POST',
-    //     body: formdata,
-    //     redirect: 'follow'
+    const apiUrl = 'https://api.waifu.pics/many/sfw/waifu';
+    var formdata = new FormData();
+    formdata.append("exclude", "[]");
+
+    var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    // Online Mode - Use API
+    const fetchImages = async () => {
+        try {
+            const response = await fetch(apiUrl, requestOptions)
+            const data = await response.json();
+            setImages(data.files);
+            setLoading(false);
+        } catch (e) {
+            console.error('An error occurred:', e.message);
+            setLoading(false);
+        }
+    }
+
+    // Offlane Mode - Use Local Files (Testing Purposes)
+    // const fetchImages = async () => {
+    //     try {
+    //         const imageUrls = await Promise.all(imageFiles.map((path) => importImage(path)));
+    //         setImages(imageUrls);
+    //         setLoading(false);
+    //     } catch (error) {
+    //         console.error('Error fetching images:', error.message);
+    //     }
     // };
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             // const response = await fetch(apiUrl, requestOptions)
-    //             // const data = await response.json();
-    //             const data = []
-    //             console.log(data.files)
-    //             setImages(data.files);
-    //             setLoading(false);
-    //         } catch (e) {
-    //             console.error('An error occurred:', e.message);
-    //             setLoading(false);
-    //         }
-    //     }
-    //     fetchData();
-    // }, [gameOver])
+    useEffect(() => {
+        fetchImages();
+    }, []);
 
     const Picture = ({image, index}) => {
         return (
@@ -86,34 +97,6 @@ function App() {
         return <div className={"text-white"} data-testid="game-over">Game Over</div>;
     }
 
-    // click image
-    // check if it belongs to one of the ones that has been clicked already
-    // if it hasn't been clicked yet
-    // increase score by 1
-    // add to state that tracks which ones have been clicked already
-    // shuffle images
-    // if it has been clinked already
-    // end game
-    // prevent any of the items from being clicked
-    // restart game button is now new game
-
-    // restart game / continue button
-    // fetch new images
-    // reset score
-
-    const fetchImages = async () => {
-        try {
-            const imageUrls = await Promise.all(imageFiles.map((path) => importImage(path)));
-            setImages(imageUrls);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching images:', error.message);
-        }
-    };
-
-    useEffect(() => {
-        fetchImages();
-    }, []);
 
     const handlePictureOnClick = (image) => {
         if (gameOver) {
